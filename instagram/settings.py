35a33b8bb6eb4b14f9e7ee7e685596de69c4290e
HyoJip <mightling7@naver.com>
2020-07-24 20:57:04 +0900

    헤로쿠 배포 준비

--- instagram/settings.py
+++ instagram/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '(m5b6(=wo8ex6rrdhc%ac2%l5e14$%y5&bo5nlj(xe+2==(tbt'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
