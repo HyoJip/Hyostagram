bf722bc50ace9e929346112b85514426f9e2e091
HyoJip <mightling7@naver.com>
2020-07-24 21:12:54 +0900

    ALLOWED_HOSE 변경

--- instagram/settings.py
+++ instagram/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stark-ridge-05336.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
