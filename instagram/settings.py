83a62c27fef1b1413a9275858c02d63a097c2609
HyoJip <mightling7@naver.com>
2020-07-24 21:04:03 +0900

    배포를 위해 정적파일 한곳으로 모음

--- instagram/settings.py
+++ instagram/settings.py
@@ -140,9 +140,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "node_modules"),
 ]
-STATIC_ROOT = os.path.join("staticfiles")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
